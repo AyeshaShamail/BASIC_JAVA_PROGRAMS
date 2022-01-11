9abd1d4002fa1a77b0cb16209c126edba653e298
AyeshaShamail <ayeshashamail1931@gmail.com>
2022-01-11 19:37:26 +0530

    adding new program--Swapping of two numbers

---
+++ src/com/ayesha/FindFactorial.java
@@ -0,0 +1,19 @@
+package com.ayesha;
+
+import java.util.Scanner;
+
+public class FindFactorial {
+
+        public static void main(String[] args){
+            int i,a, factorial=1;
+            System.out.println("Enter the number:");
+            Scanner sn= new Scanner(System.in);
+            a= sn.nextInt();
+
+            for(i = 1; i<=a; i++) {
+                factorial = factorial * i;
+            }
+            System.out.println("The result is: "+factorial);
+        }
+    }
+
