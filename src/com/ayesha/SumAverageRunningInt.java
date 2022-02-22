3add6071f041767719b42b7b70c735890e8d317d
AyeshaShamail <ayeshashamail1931@gmail.com>
2022-02-22 11:50:06 +0530

    code using do-while and do-while loop

--- src/com/ayesha/SumAverageRunningInt.java
+++ src/com/ayesha/SumAverageRunningInt.java
@@ -6,18 +6,34 @@ public class SumAverageRunningInt {
     public static void main (String[] args) {
         int sum = 0;
         double average;
-        final int LOWERBOUND = 50;
-        final int UPPERBOUND = 200;
+        final int LOWERBOUND = 1;
+        final int UPPERBOUND = 100;
 
         // Use a for-loop to sum from lowerbound to upperbound
-        for (int number = LOWERBOUND; number <= UPPERBOUND; ++number) {
+       /* for (int number = LOWERBOUND; number <= UPPERBOUND; ++number) {
             // The loop index variable number = 1, 2, 3, ..., 99, 100
             sum += number;     // same as "sum = sum + number"
-        }
+        } */
+
+        // using while-do loop
+      /*  int number = LOWERBOUND;
+        while (number <= UPPERBOUND) {
+            sum += number;
+            ++number;
+        } */
+
+        // using do-while loop
+        int number = LOWERBOUND;         // declare and init loop index variable
+        do {
+            sum += number;
+            ++number;                     // update
+        } while (number <= UPPERBOUND);
+
+
         // Compute average in double. Beware that int / int produces int!
       average= sum/(LOWERBOUND+UPPERBOUND);
-        System.out.println("The sum value is: " +sum);
-        System.out.println("The Average value is: " +average);
+        System.out.println("The SUM is: " +sum);
+        System.out.println("The AVERAGE is: " +average);
     }
 }
 
